ed2cc7da44a63c56fe2c685840abad3d11efda67
AValenzue <zuellagraphy@gmail.com>
2024-05-02 15:02:08 +0200

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter } from 'react-router-dom'
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -7,6 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
